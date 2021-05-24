dc96b827428800fc4f0a75788248eaf5ad9d46a5
duanzheng0210 <1019529641@qq.com>
2021-05-24 14:48:39 +0800

    'navbar'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,33 @@
 <template>
-    <h2>Home</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #nav-center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+    import NavBar from "@/components/common/navbar/NavBar";
+
     export default{
-        name: "Home"
+        name: "Home",
+        components:{
+          NavBar
+        }
     }
 </script>
 
 <style lang="less" scoped="scoped">
-	
+.home-nav {
+  color: #fff;
+  background-color: deeppink;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+
 </style>
